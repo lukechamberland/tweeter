771a0858711b19902c9642668d7bd9c784577c58
Luke Chamberland <luke.chamberland@shaw.ca>
2023-03-27 23:36:11 +0000

    implament renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  const createTweetElement = function(tweetData) {
+    const $tweet = $(`
+    <article>
+    <header>
+      <img src="/images/profile-hex.png">
+      <h3>${tweetData.user.name}</h3>
+    </header>
+    <section class="tweet-text">${tweetData.content.text}</section>
+    <footer>
+      <span>
+        ${timeago.format(tweetData.created_at)}
+      </span>
+      <div class="icons">
+        <i class="fa-regular fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fa-regular fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+    `);
+    return $tweet;
+  }
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+  $("#tweets-container").append($tweet);
+
+  }
+}
+
+const loadTweets = function() {
+  $.ajax({
+    type: "GET",
+    url: "/tweets",
+  }).then(function(data) {
+  renderTweets(data);
+  });
+}
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+    
+    if ($( this ).find("textarea").val() === "" || $( this ).find("textarea").val() === null) {
+      alert("Tweet box cannot be left empty");
+    }
+
+    $.ajax({
+      type: "POST",
+      url: '/tweets',
+      data: $(this).serialize(),
+    }).then(function() {
+      $("#tweets-container").empty();
+      loadTweets();
+    });
+  });
+  loadTweets();
+})
