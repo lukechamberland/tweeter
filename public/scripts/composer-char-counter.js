6b634ef0626652d03a1dbdc64bc3498c89e45ca0
Luke Chamberland <luke.chamberland@shaw.ca>
2023-03-28 22:25:44 +0000

    center avatar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  console.log('Test');
   $("#tweet-text").keydown(function() {
     let str = $(this).val()
     let counter = 140 - str.length
@@ -10,5 +9,11 @@ $(document).ready(function() {
       $(".counter").css("color", "black")
     }
   })
+  $('#tweet-text').keyup(function() {
+    var characterCount = $(this).val().length;
+    if (characterCount > 140) {
+      $('.error-message').css("display", "show")
+    }
+  });
 });
 
